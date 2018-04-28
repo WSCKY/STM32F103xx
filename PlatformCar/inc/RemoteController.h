950179d8c75c26b4b0ac40937076c901be1252bc
kyChu <hello.kychu@gmail.com>
2018-04-28 14:04:49 +0800

    update Remote Controller Driver && set RC connection to UART4

--- PlatformCar/inc/RemoteController.h
+++ PlatformCar/inc/RemoteController.h
@@ -3,16 +3,23 @@
 
 #include "stm32f10x_usart.h"
 
-#define RC_USART                    USART3
-#define RC_USART_GPIO               GPIOB
-#define RC_USART_GPIO_CLK           RCC_APB2Periph_GPIOB
-#define RC_USART_CLK                RCC_APB1Periph_USART3
+#define RC_USART                    UART4
+#define RC_USART_GPIO               GPIOC
+#define RC_USART_GPIO_CLK           RCC_APB2Periph_GPIOC
+#define RC_USART_CLK                RCC_APB1Periph_UART4
+#define RC_USART_DMA_CLK            RCC_AHBPeriph_DMA2
 #define RC_USART_RxPin              GPIO_Pin_11
 #define RC_USART_TxPin              GPIO_Pin_10
-#define RC_USART_IRQn               USART3_IRQn
-#define RC_USART_IRQHandler         USART3_IRQHandler
+#define RC_USART_Tx_DMA_Channel     DMA2_Channel5
+#define RC_USART_Tx_DMA_FLAG        DMA2_FLAG_TC5
+#define RC_USART_DR_Base            0x40004C04
+#define RC_USART_IRQn               UART4_IRQn
+#define RC_USART_IRQHandler         UART4_IRQHandler
+#define RC_USART_Tx_DMA_IRQn        DMA2_Channel4_5_IRQn
+#define RC_USART_Tx_DMA_IRQHandler  DMA2_Channel4_5_IRQHandler
 
 void RemoteControlInit(void);
-uint8_t GetRCUpdateFlag(void);
+void RCPortReceivedDataCallBack(uint8_t Data);
+void RCPortSendBytesDMA(uint8_t *p, uint32_t l);
 
 #endif /* __REMOTECONTROLLER_H */
--- PlatformCar/src/RemoteController.c
+++ PlatformCar/src/RemoteController.c
@@ -1,9 +1,10 @@
 #include "RemoteController.h"
 
-static uint8_t RCDataUpdate = 0;
+static uint8_t tx_comp_flag = 1;
+static DMA_InitTypeDef DMA_InitStructure;
 
+static void dma_config(void);
 static void usart_config(void);
-static void _rc_data_decode(uint8_t data);
 
 void RemoteControlInit(void)
 {
@@ -23,6 +24,7 @@ void RemoteControlInit(void)
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
   GPIO_Init(RC_USART_GPIO, &GPIO_InitStructure);
 
+	dma_config();
 	usart_config();
 }
 
@@ -36,14 +38,13 @@ static void usart_config(void)
 
 	/* RC USART configuration -------------------------------------------*/
   /* RC USART configured as follow:
-        - BaudRate = 100000
+        - BaudRate = 115200
         - Word Length = 8 Bits
-        - Two Stop Bit
-        - Even parity
+        - One Stop Bit
+        - No parity
         - Hardware flow control disabled (RTS and CTS signals)
         - Receive and transmit enabled
   */
-
   USART_InitStructure.USART_BaudRate = 115200;
   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
   USART_InitStructure.USART_StopBits = USART_StopBits_1;
@@ -55,10 +56,13 @@ static void usart_config(void)
 
 	/* Enable the RC_USART Interrupt */
   NVIC_InitStructure.NVIC_IRQChannel = RC_USART_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
+
+	/* Enable RC_USART DMA TX request */
+  USART_DMACmd(RC_USART, USART_DMAReq_Tx, ENABLE);
 
   /* Enable the RC_USART Receive Interrupt */
   USART_ITConfig(RC_USART, USART_IT_RXNE, ENABLE);
@@ -67,20 +71,74 @@ static void usart_config(void)
   USART_Cmd(RC_USART, ENABLE);
 }
 
-uint8_t GetRCUpdateFlag(void)
+static void dma_config(void)
 {
-	if(RCDataUpdate) {
-		RCDataUpdate = 0;
-		return 1;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	/* DMA clock enable */
+  RCC_AHBPeriphClockCmd(RC_USART_DMA_CLK, ENABLE);
+
+	/* RC_USART_Tx_DMA_Channel (triggered by RC_USART Tx event) Config */
+  DMA_DeInit(RC_USART_Tx_DMA_Channel);
+  DMA_InitStructure.DMA_PeripheralBaseAddr = RC_USART_DR_Base;
+  DMA_InitStructure.DMA_MemoryBaseAddr = 0;
+  DMA_InitStructure.DMA_DIR = DMA_DIR_PeripheralDST;
+  DMA_InitStructure.DMA_BufferSize = 0;
+  DMA_InitStructure.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+  DMA_InitStructure.DMA_MemoryInc = DMA_MemoryInc_Enable;
+  DMA_InitStructure.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+  DMA_InitStructure.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
+  DMA_InitStructure.DMA_Mode = DMA_Mode_Normal;
+  DMA_InitStructure.DMA_Priority = DMA_Priority_High;
+  DMA_InitStructure.DMA_M2M = DMA_M2M_Disable;
+  DMA_Init(RC_USART_Tx_DMA_Channel, &DMA_InitStructure);
+
+	/* Enable the RC_USART_Tx_DMA_IRQn Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = RC_USART_Tx_DMA_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+	DMA_ITConfig(RC_USART_Tx_DMA_Channel, DMA_IT_TC, ENABLE);
+
+//	/* Enable RC_USART DMA TX Channel */
+//  DMA_Cmd(RC_USART_Tx_DMA_Channel, ENABLE);
+}
+
+/*
+ * Send any bytes through rc port use dma.
+ */
+void RCPortSendBytesDMA(uint8_t *p, uint32_t l)
+{
+	if(tx_comp_flag == 1) {
+		tx_comp_flag = 0;
+
+		DMA_InitStructure.DMA_MemoryBaseAddr = (uint32_t)p;
+		DMA_InitStructure.DMA_BufferSize = l;
+		DMA_Init(RC_USART_Tx_DMA_Channel, &DMA_InitStructure);
+		/* Enable RC_USART DMA TX Channel */
+		DMA_Cmd(RC_USART_Tx_DMA_Channel, ENABLE);
 	}
-	return 0;
 }
 
 void RC_USART_IRQHandler(void)
 {
 	if(SET == USART_GetITStatus(RC_USART, USART_IT_RXNE)) {
-		_rc_data_decode(USART_ReceiveData(RC_USART));
+		RCPortReceivedDataCallBack(RC_USART->DR);
 		USART_ClearFlag(RC_USART, USART_FLAG_RXNE);
-		USART_ClearITPendingBit(RC_USART, USART_IT_RXNE);
+	}
+}
+
+/*
+ * this function handles UART DMA Interrupt.
+ */
+void RC_USART_Tx_DMA_IRQHandler(void)
+{
+	if(SET == DMA_GetFlagStatus(RC_USART_Tx_DMA_FLAG)) {
+		tx_comp_flag = 1;
+		/* Disable DEBUG_USART DMA TX Channel */
+		DMA_Cmd(RC_USART_Tx_DMA_Channel, DISABLE);
+		DMA_ClearFlag(RC_USART_Tx_DMA_FLAG);
 	}
 }
