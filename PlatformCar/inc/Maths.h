b9c146441223af0b424c91279b8a176a006f0a1c
kyChu <hello.kychu@gmail.com>
2018-04-28 14:00:17 +0800

    add CRC8 function

--- PlatformCar/inc/Maths.h
+++ PlatformCar/inc/Maths.h
@@ -4,6 +4,8 @@
 #include <math.h>
 #include <stdint.h>
 
+#define CRC_INIT                       (0x66)
+
 #define LIMIT_MAX(x, y)                (((x) > (y)) ? (y) : (x))
 #define LIMIT_MIN(x, y)                (((x) < (y)) ? (y) : (x))
 #define LIMIT_RANGE(x, max, min)       (LIMIT_MAX(LIMIT_MIN((x), (min)), (max)))
@@ -15,5 +17,6 @@ typedef union {
 
 float apply_deadband(float value, float deadband);
 void step_change(float *in, float target, float step, float deadBand);
+uint8_t Get_CRC8_Check_Sum(uint8_t *pchMessage, uint32_t dwLength, uint8_t ucCRC8);
 
 #endif /* __MATHS_H */
