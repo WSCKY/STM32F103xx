a4f3d0fbd1fca54e1802aa25e065ab045d8faa98
kyChu <hello.kychu@gmail.com>
2018-11-08 18:11:25 +0800

    new config for 110Kbps

--- dw_twr_test/src/main.c
+++ dw_twr_test/src/main.c
@@ -36,6 +36,18 @@ static dwt_config_t config = {
     DWT_PHRMODE_STD,  /* PHY header mode. */
     (129 + 8 - 8)     /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
 };
+//static dwt_config_t config = {
+//    2,               /* Channel number. */
+//    DWT_PRF_64M,     /* Pulse repetition frequency. */
+//    DWT_PLEN_1024,   /* Preamble length. Used in TX only. */
+//    DWT_PAC32,       /* Preamble acquisition chunk size. Used in RX only. */
+//    9,               /* TX preamble code. Used in TX only. */
+//    9,               /* RX preamble code. Used in RX only. */
+//    1,               /* 0 to use standard SFD, 1 to use non-standard SFD. */
+//    DWT_BR_110K,     /* Data rate. */
+//    DWT_PHRMODE_STD, /* PHY header mode. */
+//    (1025 + 64 - 32) /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+//};
 
 /* Preamble timeout, in multiple of PAC size. See NOTE 3 below. */
 #define PRE_TIMEOUT 1000
