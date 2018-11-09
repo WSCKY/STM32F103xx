468e30518c83cb0470affb9215d0aa5c4dbde59c
kyChu <hello.kychu@gmail.com>
2018-11-09 14:01:13 +0800

    add __packed self definition

--- myRTLS/RTLS/FrameStruct.h
+++ myRTLS/RTLS/FrameStruct.h
@@ -3,6 +3,19 @@
 
 #include "PlatformConfig.h"
 
+#ifndef __packed
+  /* __packed keyword used to decrease the data type alignment to 1-byte */
+  #if defined (__CC_ARM)         /* ARM Compiler */
+    #define __packed    __packed
+  #elif defined (__ICCARM__)     /* IAR Compiler */
+    #define __packed    __packed
+  #elif defined   ( __GNUC__ )   /* GNU Compiler */                        
+    #define __packed    __attribute__ ((__packed__))
+  #elif defined   (__TASKING__)  /* TASKING Compiler */
+    #define __packed    __unaligned
+  #endif /* __CC_ARM */
+#endif
+
 #define FRAME_MAX_LENGTH               (40)
 
 #define DEFAULT_STX                    (0x5A)
@@ -13,17 +26,17 @@ typedef enum {
   final_msg = 0x3
 } FrameTypeDef;
 
-typedef struct {
+__packed typedef struct {
   uint8_t TagAddr;
 } PollMsgDataDef;
 
-typedef struct {
+__packed typedef struct {
   uint8_t TagAddr;
   uint8_t AncAddr;
   FloatUnion Distance;
 } RespMsgDataDef;
 
-typedef struct {
+__packed typedef struct {
   uint8_t TagAddr;
   uint64_t x;
 } FinalMsgDataDef;
