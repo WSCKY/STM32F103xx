8cb3948ad488c0fae996baff63feeecada8e96f5
kyChu <hello.kychu@gmail.com>
2018-11-13 13:34:51 +0800

    add new parameter

--- myRTLS/RTLS/AncMain.h
+++ myRTLS/RTLS/AncMain.h
@@ -17,6 +17,8 @@
 /* This is the delay from Frame RX timestamp to TX reply timestamp used for calculating/setting the DW1000's delayed TX function. This includes the
  * frame length of approximately 2.46 ms with above configuration. */
 #define POLL_RX_TO_RESP_TX_DLY_UUS 2600
+/* This is the delay wait for TAG process the resp message. */
+#define TAG_PROC_RESP_RX_DLY_UUS 500
 /* This is the delay from the end of the frame transmission to the enable of the receiver, as programmed for the DW1000's wait for response feature. */
 #define RESP_TX_TO_FINAL_RX_DLY_UUS 500
 /* Receive final timeout. See NOTE 5 below. */
