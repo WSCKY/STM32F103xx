07cb11a53d3258c97e0fc57c63cd41a24fec00b0
kyChu <hello.kychu@gmail.com>
2018-11-14 15:23:55 +0800

    move normal type definition to ComDataTypes.h

--- myRTLS/inc/CommProtocol.h
+++ myRTLS/inc/CommProtocol.h
@@ -4,41 +4,6 @@
 #include "ComIf_conf.h"
 #include "ComVersion.h"
 
-/* exact-width signed integer types */
-#ifndef int8_t
-typedef   signed          char int8_t;
-#endif
-#ifndef int16_t
-typedef   signed short     int int16_t;
-#endif
-#ifndef int32_t
-typedef   signed           int int32_t;
-#endif
-
-/* exact-width unsigned integer types */
-#ifndef uint8_t
-typedef unsigned          char uint8_t;
-#endif
-#ifndef uint16_t
-typedef unsigned short     int uint16_t;
-#endif
-#ifndef uint32_t
-typedef unsigned           int uint32_t;
-#endif
-
-#ifndef __packed
-  /* __packed keyword used to decrease the data type alignment to 1-byte */
-  #if defined (__CC_ARM)         /* ARM Compiler */
-    #define __packed    __packed
-  #elif defined (__ICCARM__)     /* IAR Compiler */
-    #define __packed    __packed
-  #elif defined   ( __GNUC__ )   /* GNU Compiler */                        
-    #define __packed    __attribute__ ((__packed__))
-  #elif defined   (__TASKING__)  /* TASKING Compiler */
-    #define __packed    __unaligned
-  #endif /* __CC_ARM */
-#endif
-
 #define Header1              (0x55)
 #define Header2              (0xAA)
 
