dd7e30ee42fa1655ebdd08d8c831e89dc79027ac
kyChu <hello.kychu@gmail.com>
2018-11-14 15:21:09 +0800

    add new package

--- myRTLS/inc/ComIf_conf.h
+++ myRTLS/inc/ComIf_conf.h
@@ -3,7 +3,7 @@
 
 #include "SysConfig.h"
 
-#include "DataTypes.h"
+#include "ComDataTypes.h"
 
 #include "usbd_usr.h"
 #include "usbd_cdc_vcp.h"
@@ -11,8 +11,10 @@
 #define COM_IF_TX_BYTES(p, l)          USB_CDC_SendBufferFast(p, l)
 #define COM_IF_TX_CHECK()              USBD_isEnabled()
 
-//#define COM_USER_TYPE                  TYPE_DIST_Response = 0x11,
+#define COM_USER_TYPE                  TYPE_DIST_Response = 0x11, \
+                                       TYPE_DIST_GROUP_Resp = 0x12,
 
-//#define COM_USER_TYPE_DATA             DistDataRespDef DistData;
+#define COM_USER_TYPE_DATA             DistDataRespDef DistData; \
+                                       DistGroupDataRespDef DistGroup;
 
 #endif /* __COMIF_CONF_H */
